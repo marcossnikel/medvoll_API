806b95eecea9076385e312dc26889eb50e4630da
marcossnikel <marcosnikeldev@gmail.com>
2024-07-09 23:20:12 -0300

    feat: add dto to post doctors request

--- src/main/java/med/voll/api/doctor/DataRegisterDoctor.java
+++ src/main/java/med/voll/api/doctor/DataRegisterDoctor.java
@@ -1,6 +1,31 @@
 package med.voll.api.doctor;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.address.DataAddress;
 
-public record DataRegisterDoctor(String name, String email, String crm, Specialty specialty, DataAddress address) {
-}
+public record DataRegisterDoctor(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String phone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+
+        @NotNull
+        Specialty specialty,
+
+        @NotNull @Valid
+        DataAddress address
+){}
