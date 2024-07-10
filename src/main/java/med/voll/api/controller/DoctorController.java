c3711daf17a544782d00eb82f5c86b5826daa7a7
marcossnikel <marcosnikeldev@gmail.com>
2024-07-09 23:20:38 -0300

    feat: add new get request

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,15 +1,20 @@
 package med.voll.api.controller;
 
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.address.Address;
+import med.voll.api.doctor.DataListDoctor;
 import med.voll.api.doctor.DataRegisterDoctor;
 import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("doctors")
@@ -19,7 +24,13 @@ public class DoctorController {
     private DoctorRepository repository;
 
     @PostMapping
-    public void register(@RequestBody DataRegisterDoctor data) {
+    @Transactional
+    public void register(@RequestBody @Valid DataRegisterDoctor data) {
         repository.save(new Doctor(data));
+    }
+
+    @GetMapping
+    public Page<DataListDoctor> getDoctors(@PageableDefault(size = 10, sort= {"name"}) Pageable pagination) {
+        return repository.findAll(pagination).map(DataListDoctor::new);
     }
 }
